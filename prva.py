8e235fc69f2e66c73d2822d3ccdf0961046bc56a
pijacka1337 <old.ikec@gmail.com>
2023-12-09 14:02:39 +0100

    Add files via upload

---
+++ prva.py
@@ -0,0 +1,31 @@
+with open("02/in1.txt", "r") as f:
+    lines = f.readlines()
+    sum = 0
+    num_red = 12
+    num_green = 13
+    num_blue = 14
+    for line in lines:
+        max_red = 0
+        max_green = 0
+        max_blue = 0
+        
+        data = line.replace("\n", "").replace(";", "").replace(",", "").split(" ")
+        id = int(data[1][:-1])
+
+        for i in range(2, len(data), 2):
+            #print(data[i], data[i+1])
+            if(data[i+1] == "red"):
+                if(int(data[i]) > max_red):
+                    max_red = int(data[i])
+            if(data[i+1] == "green"):
+                if(int(data[i]) > max_green):
+                    max_green = int(data[i])
+            if(data[i+1] == "blue"):
+                if(int(data[i]) > max_blue):
+                    max_blue = int(data[i])
+        if(max_red <= num_red and max_green <= num_green and max_blue <= num_blue):
+            sum += id
+
+            print(id, max_red, max_green, max_blue)    
+
+    print(sum)
